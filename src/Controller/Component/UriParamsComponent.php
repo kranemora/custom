6e9c0cafe82f6222efb7a39f132833fed28316c7
Krane <kranemora@gmail.com>
2018-07-20 12:19:02 -0500

    Fixed 6: Actualización de instrucciones marcadas como deprecadas en componente UriParams.

--- src/Controller/Component/UriParamsComponent.php
+++ src/Controller/Component/UriParamsComponent.php
@@ -12,13 +12,13 @@ class UriParamsComponent extends Component
         $controller = $this->_registry->getController();
 
         if (!empty($controller->request->getData())) {
-			$controller->request->data = Sanitizer::clean($controller->request->getData());
+            $controller->request = $controller->request->withParsedBody(Sanitizer::clean($controller->request->getData()));
         }
-        if (!empty($controller->request->getQuery())) {
-			$controller->request->query = Sanitizer::clean($controller->request->getQuery());
+        if (!empty($controller->request->getQueryParams())) {
+			$controller->request = $controller->request->withQueryParams(Sanitizer::clean($controller->request->getQueryParams()));
 		}
         if (!empty($controller->request->getParam('pass'))) {
-			$controller->request->params['pass'] = Sanitizer::clean($controller->request->getParam('pass'));
+			$controller->request = $controller->request->withParam('pass', Sanitizer::clean($controller->request->getParam('pass')));
 		}
     }
 }
