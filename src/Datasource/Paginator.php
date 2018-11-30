0c8586bdaac96a5143d28a0a1b64e13c96006a87
Krane <kranemora@gmail.com>
2018-11-30 15:20:32 -0500

    Fixed 7: Corrección de código por actualización de framework.

--- src/Datasource/Paginator.php
+++ src/Datasource/Paginator.php
@@ -17,9 +17,114 @@ namespace Custom\Datasource;
 class Paginator extends \Cake\Datasource\Paginator
 {
 
+    /**
+     * Handles automatic pagination of model records.
+     *
+     * ### Configuring pagination
+     *
+     * When calling `paginate()` you can use the $settings parameter to pass in
+     * pagination settings. These settings are used to build the queries made
+     * and control other pagination settings.
+     *
+     * If your settings contain a key with the current table's alias. The data
+     * inside that key will be used. Otherwise the top level configuration will
+     * be used.
+     *
+     * ```
+     *  $settings = [
+     *    'limit' => 20,
+     *    'maxLimit' => 100
+     *  ];
+     *  $results = $paginator->paginate($table, $settings);
+     * ```
+     *
+     * The above settings will be used to paginate any repository. You can configure
+     * repository specific settings by keying the settings with the repository alias.
+     *
+     * ```
+     *  $settings = [
+     *    'Articles' => [
+     *      'limit' => 20,
+     *      'maxLimit' => 100
+     *    ],
+     *    'Comments' => [ ... ]
+     *  ];
+     *  $results = $paginator->paginate($table, $settings);
+     * ```
+     *
+     * This would allow you to have different pagination settings for
+     * `Articles` and `Comments` repositories.
+     *
+     * ### Controlling sort fields
+     *
+     * By default CakePHP will automatically allow sorting on any column on the
+     * repository object being paginated. Often times you will want to allow
+     * sorting on either associated columns or calculated fields. In these cases
+     * you will need to define a whitelist of all the columns you wish to allow
+     * sorting on. You can define the whitelist in the `$settings` parameter:
+     *
+     * ```
+     * $settings = [
+     *   'Articles' => [
+     *     'finder' => 'custom',
+     *     'sortWhitelist' => ['title', 'author_id', 'comment_count'],
+     *   ]
+     * ];
+     * ```
+     *
+     * Passing an empty array as whitelist disallows sorting altogether.
+     *
+     * ### Paginating with custom finders
+     *
+     * You can paginate with any find type defined on your table using the
+     * `finder` option.
+     *
+     * ```
+     *  $settings = [
+     *    'Articles' => [
+     *      'finder' => 'popular'
+     *    ]
+     *  ];
+     *  $results = $paginator->paginate($table, $settings);
+     * ```
+     *
+     * Would paginate using the `find('popular')` method.
+     *
+     * You can also pass an already created instance of a query to this method:
+     *
+     * ```
+     * $query = $this->Articles->find('popular')->matching('Tags', function ($q) {
+     *   return $q->where(['name' => 'CakePHP'])
+     * });
+     * $results = $paginator->paginate($query);
+     * ```
+     *
+     * ### Scoping Request parameters
+     *
+     * By using request parameter scopes you can paginate multiple queries in
+     * the same controller action:
+     *
+     * ```
+     * $articles = $paginator->paginate($articlesQuery, ['scope' => 'articles']);
+     * $tags = $paginator->paginate($tagsQuery, ['scope' => 'tags']);
+     * ```
+     *
+     * Each of the above queries will use different query string parameter sets
+     * for pagination data. An example URL paginating both results would be:
+     *
+     * ```
+     * /dashboard?articles[page]=1&tags[page]=2
+     * ```
+     *
+     * @param \Cake\Datasource\RepositoryInterface|\Cake\Datasource\QueryInterface $object The table or query to paginate.
+     * @param array $params Request params
+     * @param array $settings The settings/configuration used for pagination.
+     * @return \Cake\Datasource\ResultSetInterface Query results
+     * @throws \Cake\Datasource\Exception\PageOutOfBoundsException
+     */
     public function paginate($object, array $params = [], array $settings = [])
     {
-		$query = null;
+        $query = null;
         if ($object instanceof QueryInterface) {
             $query = $object;
             $object = $query->getRepository();
@@ -40,21 +145,28 @@ class Paginator extends \Cake\Datasource\Paginator
         } else {
             $query->applyOptions($options);
         }
-		
+
         $cleanQuery = clone $query;
+		// Hack from extended. Start. Incorpora coherencia en el comportamiento de la paginación fuera de los límites.
+        // $results = $query->all();
+        // $numResults = count($results);
+		// Hack from extended. End. Incorpora coherencia en el comportamiento de la paginación fuera de los límites.
         $count = $cleanQuery->count();
+
         $page = $options['page'];
         $limit = $options['limit'];
         $pageCount = max((int)ceil($count / $limit), 1);
         $requestedPage = $page;
         $page = min($page, $pageCount);
-		
+
+		// Hack from extended. Start. Incorpora coherencia en el comportamiento de la paginación fuera de los límites.
         if ($requestedPage > $page) {
 			$query->page($page);
 		}
 
         $results = $query->all();
         $numResults = count($results);
+		// Hack from extended. End. Incorpora coherencia en el comportamiento de la paginación fuera de los límites.
 
         $order = (array)$options['order'];
         $sortDefault = $directionDefault = false;
@@ -63,24 +175,45 @@ class Paginator extends \Cake\Datasource\Paginator
             $directionDefault = current($defaults['order']);
         }
 
+        $start = 0;
+        if ($count >= 1) {
+            $start = (($page - 1) * $limit) + 1;
+        }
+        $end = $start + $limit - 1;
+        if ($count < $end) {
+            $end = $count;
+        }
+
         $paging = [
             'finder' => $finder,
             'page' => $page,
             'current' => $numResults,
             'count' => $count,
             'perPage' => $limit,
+            'start' => $start,
+            'end' => $end,
             'prevPage' => $page > 1,
             'nextPage' => $count > ($page * $limit),
             'pageCount' => $pageCount,
-            'sort' => key($order),
+            'sort' => $options['sort'],
             'direction' => current($order),
             'limit' => $defaults['limit'] != $limit ? $limit : null,
             'sortDefault' => $sortDefault,
             'directionDefault' => $directionDefault,
             'scope' => $options['scope'],
+            'completeSort' => $order,
         ];
 
         $this->_pagingParams = [$alias => $paging];
+
+		// Hack from extended. Start. Incorpora coherencia en el comportamiento de la paginación fuera de los límites.
+        //if ($requestedPage > $page) {
+        //    throw new PageOutOfBoundsException([
+        //        'requestedPage' => $requestedPage,
+        //        'pagingParams' => $this->_pagingParams
+        //    ]);
+        //}
+		// Hack from extended. End. Incorpora coherencia en el comportamiento de la paginación fuera de los límites.
 
         return $results;
     }
